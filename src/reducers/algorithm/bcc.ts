326915019c60e838b733ebdf49a836f564340f4e
Laszlo Korte <me@laszlokorte.de>
2020-02-01 16:37:01 +0100

    add Bi-Connected-Component algorithm

--- src/reducers/algorithm/bcc.ts
+++ src/reducers/algorithm/bcc.ts
@@ -7,19 +7,107 @@ const run = (graph) => {
 
     track(state);
 
-    return; ///////////// TODO
+    const stack = [];
+
+    const nodes = makeSymmetric(graph.nodes)
+
+    for(let i=0;i<nodes.length;i++) {
+        if(state.nodes.discovery[i] === 0) {
+            visitNode(state, nodes, i, null, stack, track);
+        }
+    }
+
+    console.log(state.edges.component)
+
+    state.edges.color = state.edges.component.map((e) =>
+        e.map((c) => `hsl(${c*360 / state.currentComponent}, 100%, 50%)`)
+    )
+
+    track(state)
 
     return steps;
+}
+
+const makeSymmetric = (nodes) => {
+    const newNodes = nodes.map((n) => n.map((m) => m))
+
+    for(let a=0;a<nodes.length;a++) {
+        const neighbours = nodes[a]
+        for(let b=0;b<neighbours.length;b++) {
+            const neighbour = neighbours[b]
+            newNodes[neighbour].push(a)
+        }
+    }
+
+    return newNodes
 }
 
 const init = (graph) => {
     return {
         nodes: {
+            lowpoint: graph.nodes.map(() => 0),
+            discovery: graph.nodes.map(() => 0),
+            color: graph.nodes.map(() => 'WHITE'),
         },
         edges: {
+            color: graph.nodes.map((n) => n.map(() => null)),
+            component: graph.nodes.map((n) => n.map(() => 0)),
         },
         time: 0,
+        currentComponent: 1,
     }
+}
+
+const visitNode = (state, nodes, nodeId, parent, stack, track) => {
+    state.time++;
+    state.nodes.discovery[nodeId] = state.time;
+    state.nodes.lowpoint[nodeId] = state.time;
+    let numberOfComponents = 0;
+
+    track(state);
+
+    for(let i=0;i<nodes[nodeId].length;i++) {
+        const neighbour = nodes[nodeId][i];
+        const owner = nodeId < neighbour ? nodeId : neighbour
+        const ownerIndex = nodeId < neighbour ? i : nodes[neighbour].indexOf(nodeId)
+
+        if(state.nodes.discovery[neighbour] === 0) {
+           stack.push({node: owner, edgeIndex: ownerIndex})
+           visitNode(state, nodes, neighbour, nodeId, stack, track)
+
+           state.nodes.lowpoint[nodeId] = Math.min(
+               state.nodes.lowpoint[nodeId],
+               state.nodes.lowpoint[neighbour]
+           );
+
+           if(state.nodes.lowpoint[neighbour] >= state.nodes.discovery[nodeId]) {
+               let e
+               do {
+                   e = stack.pop()
+                   if(e) {
+                       state.edges.component[e.node][e.edgeIndex] = state.currentComponent
+                   }
+               } while(e && !(e.node === owner && e.index === ownerIndex))
+               state.currentComponent += 1
+               numberOfComponents += 1
+
+               track(state);
+           }
+        } else if(nodeId !== parent && state.nodes.discovery[neighbour] < state.nodes.discovery[nodeId]) {
+           stack.push({node: owner, edgeIndex: ownerIndex})
+
+           state.nodes.lowpoint[nodeId] = Math.min(
+               state.nodes.lowpoint[nodeId],
+               state.nodes.discovery[neighbour]
+           );
+        }
+    }
+
+    if(numberOfComponents > 1 || (numberOfComponents > 0 && parent !== null)) {
+       state.nodes.color[nodeId] = 'BLACK'
+    }
+
+    track(state);
 }
 
 const copy = (object) => {
@@ -29,13 +117,15 @@ const copy = (object) => {
 
 export default {
     run,
-    name: "❌ Bi-Connected Components",
+    name: "Bi-Connected-Components",
     parameters: {
     },
     dependencies: {
-        nodes: ['position'],
+        nodes: [],
         edges: [],
     },
     requirements: {
+        multiGraph: false,
+        directed: false,
     },
 }
--- src/reducers/storage.ts
+++ src/reducers/storage.ts
@@ -580,6 +580,431 @@ const store = {
         }
       }
     }
+  },
+  "Undirected Example":  {
+    "nodes": [
+      [
+        1,
+        3
+      ],
+      [
+        2
+      ],
+      [
+        3
+      ],
+      [
+        4
+      ],
+      [
+        5,
+        6
+      ],
+      [
+        6,
+        7,
+        9,
+        10
+      ],
+      [],
+      [
+        8
+      ],
+      [
+        9
+      ],
+      [],
+      [
+        11,
+        12
+      ],
+      [
+        12
+      ],
+      []
+    ],
+    "attributes": {
+      "edges": {
+        "label": [
+          [
+            "",
+            ""
+          ],
+          [
+            ""
+          ],
+          [
+            ""
+          ],
+          [
+            ""
+          ],
+          [
+            "",
+            ""
+          ],
+          [
+            "",
+            "",
+            "",
+            ""
+          ],
+          [],
+          [
+            ""
+          ],
+          [
+            ""
+          ],
+          [],
+          [
+            "",
+            ""
+          ],
+          [
+            ""
+          ],
+          []
+        ],
+        "cost": [
+          [
+            1,
+            1
+          ],
+          [
+            1
+          ],
+          [
+            1
+          ],
+          [
+            1
+          ],
+          [
+            1,
+            1
+          ],
+          [
+            1,
+            1,
+            1,
+            1
+          ],
+          [],
+          [
+            1
+          ],
+          [
+            1
+          ],
+          [],
+          [
+            1,
+            1
+          ],
+          [
+            1
+          ],
+          []
+        ],
+        "capacity": [
+          [
+            1,
+            1
+          ],
+          [
+            1
+          ],
+          [
+            1
+          ],
+          [
+            1
+          ],
+          [
+            1,
+            1
+          ],
+          [
+            1,
+            1,
+            1,
+            1
+          ],
+          [],
+          [
+            1
+          ],
+          [
+            1
+          ],
+          [],
+          [
+            1,
+            1
+          ],
+          [
+            1
+          ],
+          []
+        ],
+        "weight": [
+          [
+            1,
+            1
+          ],
+          [
+            1
+          ],
+          [
+            1
+          ],
+          [
+            1
+          ],
+          [
+            1,
+            1
+          ],
+          [
+            1,
+            1,
+            1,
+            1
+          ],
+          [],
+          [
+            1
+          ],
+          [
+            1
+          ],
+          [],
+          [
+            1,
+            1
+          ],
+          [
+            1
+          ],
+          []
+        ]
+      },
+      "nodes": {
+        "position": [
+          {
+            "x": -143.53118896484375,
+            "y": -122.5411376953125
+          },
+          {
+            "x": 130.9080810546875,
+            "y": -80.74838256835938
+          },
+          {
+            "x": 126.728759765625,
+            "y": 165.82901000976562
+          },
+          {
+            "x": -197.86181640625,
+            "y": 170.00827026367188
+          },
+          {
+            "x": -363.63983154296875,
+            "y": 275.8833312988281
+          },
+          {
+            "x": -600.4655151367188,
+            "y": 100.35366821289062
+          },
+          {
+            "x": -639.4721069335938,
+            "y": 310.71063232421875
+          },
+          {
+            "x": -671.51318359375,
+            "y": -151.7960662841797
+          },
+          {
+            "x": -881.8701782226562,
+            "y": -118.36184692382812
+          },
+          {
+            "x": -872.1185302734375,
+            "y": 252.20074462890625
+          },
+          {
+            "x": -405.7557373046875,
+            "y": -129.0555877685547
+          },
+          {
+            "x": -227.66500854492188,
+            "y": -224.48822021484375
+          },
+          {
+            "x": -504.6457214355469,
+            "y": -255.60964965820312
+          }
+        ],
+        "label": [
+          "new",
+          "new",
+          "new",
+          "new",
+          "new",
+          "new",
+          "new",
+          "new",
+          "new",
+          "new",
+          "new",
+          "new",
+          "new"
+        ],
+        "color": [
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null
+        ],
+        "initial": [
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false
+        ],
+        "final": [
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false
+        ],
+        "source": [
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false
+        ],
+        "sink": [
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false
+        ]
+      }
+    },
+    "flags": {
+      "multiGraph": false,
+      "directed": false
+    },
+    "attributeTypes": {
+      "edges": {
+        "label": {
+          "default": "",
+          "type": "text",
+          "visible": true
+        },
+        "cost": {
+          "default": 1,
+          "type": "numeric",
+          "visible": false
+        },
+        "capacity": {
+          "default": 1,
+          "type": "numeric",
+          "visible": true
+        },
+        "weight": {
+          "default": 1,
+          "type": "numeric",
+          "visible": true
+        }
+      },
+      "nodes": {
+        "position": {
+          "default": {
+            "x": 0,
+            "y": 0
+          },
+          "type": "object",
+          "visible": false
+        },
+        "label": {
+          "default": "new",
+          "type": "text",
+          "visible": true
+        },
+        "color": {
+          "default": null,
+          "type": "color",
+          "visible": false
+        },
+        "initial": {
+          "default": false,
+          "type": "boolean",
+          "visible": false
+        },
+        "final": {
+          "default": false,
+          "type": "boolean",
+          "visible": false
+        },
+        "source": {
+          "default": false,
+          "type": "boolean",
+          "visible": false
+        },
+        "sink": {
+          "default": false,
+          "type": "boolean",
+          "visible": false
+        }
+      }
+    }
   }
 }
 
